f9c08eca7d5e93e9a85aaab45cd7209a47f4b83c
yeshodhan <yeshodhan.kulkarni@tp-link.com>
2015-11-21 15:29:52 -0800

    test cases.

---
+++ src/main/java/com/mickoo/xml/xsd2simplexml/Main.java
@@ -0,0 +1,13 @@
+package com.mickoo.xml.xsd2simplexml;
+
+/**
+ * com.mickoo.xml.xsd2simplexml
+ *
+ * @author Yeshodhan Kulkarni (yeshodhan.kulkarni@gmail.com)
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class Main {
+
+}
---
+++ src/test/java/com/example/Person.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.simpleframework.xml.Root;
+
+/**
+ * com.example
+ *
+ * @author Yeshodhan Kulkarni (yeshodhan.kulkarni@tp-link.com)
+ * @version 1.0
+ * @since 1.0
+ */
+
+@Root
+public class Person {
+
+    String firstName;
+    String lastName;
+
+}
