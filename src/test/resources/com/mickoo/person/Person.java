b4a4c2d18a50c9b643fdf85d6bb8f60b511116af
Enrico Siboni <enrico.siboni3@studio.unibo.it>
2019-01-15 16:18:16 +0100

    Updated generated classe by person.xsd; Person tests passing

--- src/test/resources/com/mickoo/person/Person.java
+++ src/test/resources/com/mickoo/person/Person.java
@@ -1,7 +1,10 @@
 
 package com.mickoo.person;
 
+import java.util.List;
+import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
+import org.simpleframework.xml.ElementList;
 import org.simpleframework.xml.Namespace;
 import org.simpleframework.xml.Root;
 
@@ -16,16 +19,38 @@ import org.simpleframework.xml.Root;
 @Namespace(reference = "http://person.mickoo.com/")
 public class Person {
 
+    @Element(name = "Email", required = false)
+    private String email;
     @Element(name = "FirstName", required = false)
     private String firstName;
     @Element(name = "LastName", required = false)
     private String lastName;
-    @Element(name = "Is_Adult", required = false)
-    private String isAdult;
-    @Element(name = "Country_Of_Birth", required = false)
-    private String countryOfBirth;
+    @Element(name = "Adult", required = false)
+    private Boolean adult;
+    @Element(name = "Addresses", required = false)
+    private Addresses addresses;
+    @Element(name = "Gender", required = false)
+    private GenderEnum gender;
+    @ElementList(name = "Favorite_Fruits", entry = "Favorite_Fruits", inline = true, required = false)
+    private List<Fruits> favoriteFruits;
+    @Element(name = "SomeThing_really_whacky-by-the-user", required = false)
+    private String someThingReallyWhackyByTheUser;
+    @Element(name = "Pets", required = false)
+    private Pets pets;
+    @ElementList(name = "Phone", entry = "Phone", inline = true, required = false)
+    private List<Phone> phone;
+    @Attribute(name = "id", required = false)
+    private String id;
 
     public Person() {
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getFirstName() {
@@ -44,20 +69,68 @@ public class Person {
         this.lastName = lastName;
     }
 
-    public String getIsAdult() {
-        return isAdult;
+    public Boolean getAdult() {
+        return adult;
     }
 
-    public void setIsAdult(String isAdult) {
-        this.isAdult = isAdult;
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
     }
 
-    public String getCountryOfBirth() {
-        return countryOfBirth;
+    public Addresses getAddresses() {
+        return addresses;
     }
 
-    public void setCountryOfBirth(String countryOfBirth) {
-        this.countryOfBirth = countryOfBirth;
+    public void setAddresses(Addresses addresses) {
+        this.addresses = addresses;
+    }
+
+    public GenderEnum getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderEnum gender) {
+        this.gender = gender;
+    }
+
+    public List<Fruits> getFavoriteFruits() {
+        return favoriteFruits;
+    }
+
+    public void setFavoriteFruits(List<Fruits> favoriteFruits) {
+        this.favoriteFruits = favoriteFruits;
+    }
+
+    public String getSomeThingReallyWhackyByTheUser() {
+        return someThingReallyWhackyByTheUser;
+    }
+
+    public void setSomeThingReallyWhackyByTheUser(String someThingReallyWhackyByTheUser) {
+        this.someThingReallyWhackyByTheUser = someThingReallyWhackyByTheUser;
+    }
+
+    public Pets getPets() {
+        return pets;
+    }
+
+    public void setPets(Pets pets) {
+        this.pets = pets;
+    }
+
+    public List<Phone> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(List<Phone> phone) {
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
 }
